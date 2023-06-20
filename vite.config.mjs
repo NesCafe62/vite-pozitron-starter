9cd0eed9889f578d82934e15755d3ca11742bbee
NesCafe62 <>
2023-06-20 18:34:04 +0400

    remove commented import

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 import pozitronPlugin from 'vite-plugin-pozitron';
-/* const pozitronPlugin = import('vite-plugin-pozitron').then(
-	(r) => r.default
-); */
-
-// throw new Error(JSON.stringify(pozitronPlugin, null, 4));
 
 export default defineConfig({
 	plugins: [pozitronPlugin()],
